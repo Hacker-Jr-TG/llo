4902371e86efefbb5b971c37e3aea41514c60862
DK BOTx TG <100774327+DKBOTx@users.noreply.github.com>
2022-05-28 00:01:58 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,12 +2,12 @@ echo "Cloning Repo...."
 if [ -z $BRANCH ]
 then
   echo "Cloning main branch...."
-  git clone https://github.com/subinps/VCPlayerBot /VCPlayerBot
+  git clone https://github.com/DKBOTx/MoosicPlayer /MoosicPlayer
 else
   echo "Cloning $BRANCH branch...."
-  git clone https://github.com/subinps/VCPlayerBot -b $BRANCH /VCPlayerBot
+  git clone https://github.com/DKBOTx/MoosicPlayer -b $BRANCH /MoosicPlayer
 fi
-cd /VCPlayerBot
+cd /MoosicPlayer
 pip3 install -U -r requirements.txt
-echo "Starting Bot...."
+echo "Starting Bot.... 𝙏𝙀𝙎𝙎𝘼"
 python3 main.py
